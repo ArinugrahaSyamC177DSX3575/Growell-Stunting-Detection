67cafc5fcfc70a608b6731eb9b30e39c1287f535
ifkusyoba <ifkusyobaa@gmail.com>
2023-06-14 15:02:40 +0700

    Adjust strings.xml for further localization.

--- Mobile Development/composeapp/app/src/main/java/com/ifkusyoba/compose_app/ui/views/hitungscreen/HitungScreen.kt
+++ Mobile Development/composeapp/app/src/main/java/com/ifkusyoba/compose_app/ui/views/hitungscreen/HitungScreen.kt
@@ -1,6 +1,5 @@
 package com.ifkusyoba.compose_app.ui.views.hitungscreen
 
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -15,24 +14,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import com.ifkusyoba.compose_app.ui.theme.ComposeAppTheme
-import com.ifkusyoba.compose_app.ui.components.EditTextCustom
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.text.isDigitsOnly
-import com.ifkusyoba.compose_app.viewmodel.MainViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.ifkusyoba.compose_app.di.Injection
-import com.ifkusyoba.compose_app.navigation.Screen
 import com.ifkusyoba.compose_app.ui.components.BottomNavigationBar
+import com.ifkusyoba.compose_app.ui.components.EditTextCustom
 import com.ifkusyoba.compose_app.ui.components.GenderDropdownCustom
 import com.ifkusyoba.compose_app.ui.components.HitungButtonCustom
-import com.ifkusyoba.compose_app.ui.views.resultscreen.HealthyConfirmationScreen
+import com.ifkusyoba.compose_app.ui.theme.ComposeAppTheme
 import com.ifkusyoba.compose_app.viewmodel.FieldType
 import com.ifkusyoba.compose_app.viewmodel.HitungViewModel
 import com.ifkusyoba.compose_app.viewmodel.ViewModelFactory
+import com.ifkusyoba.compose_app.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,49 +63,49 @@ fun HitungScreen(
                     .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.spacedBy(8.dp)
             ) {
-                Text("Selamat Datang", modifier = Modifier.align(Alignment.CenterHorizontally))
+                Text(stringResource(id = R.string.hitung_welcome), modifier = Modifier.align(Alignment.CenterHorizontally))
                 Text(
-                    "Silakan masukkan data diri Anda!",
+                    stringResource(id = R.string.hitung_welcome_subtitle),
                     modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
                 Spacer(modifier = Modifier.height(16.dp))
-                Text(text = "Nama", style = MaterialTheme.typography.bodyLarge)
+                Text(text = stringResource(id = R.string.hitung_name), style = MaterialTheme.typography.bodyLarge)
                 EditTextCustom(
                     text = viewModel.nameValue.value,
-                    label = "Masukkan nama Anda!",
+                    label = stringResource(id = R.string.hitung_name_hint),
                     onTextChange = { viewModel.onTextChange(it, FieldType.NAME) }
                 )
-                Text(text = "Jenis Kelamin", style = MaterialTheme.typography.bodyLarge)
+                Text(text = stringResource(id = R.string.hitung_jenis_kelamin), style = MaterialTheme.typography.bodyLarge)
                 GenderDropdownCustom { selectedGender ->
                     viewModel.onTextChange(selectedGender, FieldType.GENDER)
                 }
-                Text(text = "Usia", style = MaterialTheme.typography.bodyLarge)
+                Text(text = stringResource(id = R.string.hitung_usia), style = MaterialTheme.typography.bodyLarge)
                 EditTextCustom(
                     text = viewModel.ageValue.value,
-                    label = "Masukkan usia Anda!",
+                    label = stringResource(id = R.string.hitung_usia_hint),
                     onTextChange = { viewModel.onTextChange(it, FieldType.AGE) },
                     keyboardType = KeyboardType.Number
                 )
-                Text(text = "Tinggi Badan", style = MaterialTheme.typography.bodyLarge)
+                Text(text = stringResource(id = R.string.hitung_tinggi_badan), style = MaterialTheme.typography.bodyLarge)
                 EditTextCustom(
                     text = viewModel.heightValue.value,
-                    label = "Masukkan tinggi badan Anda! (cm)",
+                    label = stringResource(id = R.string.hitung_tinggi_badan_hint),
                     onTextChange = { viewModel.onTextChange(it, FieldType.HEIGHT) },
                     keyboardType = KeyboardType.Number
                 )
-                Text(text = "Berat Badan", style = MaterialTheme.typography.bodyLarge)
+                Text(text = stringResource(id = R.string.hitung_berat_badan), style = MaterialTheme.typography.bodyLarge)
                 EditTextCustom(
                     text = viewModel.weightValue.value,
-                    label = "Masukkan berat badan Anda! (kg)",
+                    label = stringResource(id = R.string.hitung_berat_badan_hint),
                     onTextChange = { viewModel.onTextChange(it, FieldType.WEIGHT) },
                     keyboardType = KeyboardType.Number
                 )
                 Spacer(modifier = Modifier.height(16.dp))
-                HitungButtonCustom(text = "Hitung", onClick = {
+                HitungButtonCustom(text = stringResource(id = R.string.hitung_submit), onClick = {
                     if (viewModel.nameValue.value.isEmpty() || viewModel.ageValue.value.isEmpty() || viewModel.heightValue.value.isEmpty() || viewModel.weightValue.value.isEmpty()) {
                         Toast.makeText(
                             context,
-                            "Mohon lengkapi data diri Anda!",
+                            R.string.hitung_validation_datadiri,
                             Toast.LENGTH_SHORT
                         )
                             .show()
@@ -115,7 +113,7 @@ fun HitungScreen(
                     } else if (!viewModel.ageValue.value.isDigitsOnly() || !viewModel.heightValue.value.isDigitsOnly() || !viewModel.weightValue.value.isDigitsOnly()) {
                         Toast.makeText(
                             context,
-                            "Umur, tinggi badan, dan berat badan harus berupa angka!",
+                            R.string.hitung_validation_angka,
                             Toast.LENGTH_SHORT
                         ).show()
                         return@HitungButtonCustom
@@ -131,7 +129,7 @@ fun HitungScreen(
                             viewModel.heightValue.value.toInt(),
                             viewModel.weightValue.value.toInt()
                         )
-                        Toast.makeText(context, "Data berhasil terkirim!", Toast.LENGTH_SHORT)
+                        Toast.makeText(context, R.string.hitung_validation_sended, Toast.LENGTH_SHORT)
                             .show()
                     }
                 })
