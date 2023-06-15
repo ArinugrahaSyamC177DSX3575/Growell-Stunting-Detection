9b42ef4d385f34d80507d83bc6d61542b763762c
AnggaSyam <128708680+ArinugrahaSyamC177DSX3575@users.noreply.github.com>
2023-06-15 11:44:50 +0800

    Update stunting.js

--- Cloud Computing/stunting-api-express/handlers/stunting.js
+++ Cloud Computing/stunting-api-express/handlers/stunting.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 // API Get All Stuntings entries
 const getAllStuntings = async (req, res) => {
     const query = 'SELECT * FROM stuntings';
-    sql.query(query, (err, result) => {
+    req.sql.query(query, (err, result) => {
       if (err) {
         console.error('Error retrieving stunting entries: ', err);
         res.status(500).json({ error: 'Internal Server Error' });
@@ -14,12 +14,13 @@ const getAllStuntings = async (req, res) => {
     });
   };
 
+/*
 //API Entry New Stunting
 const newStuntingEntry = (req, res) => {
   const { name, gender, age, height, weight } = req.body;
 
   // Melakukan permintaan POST ke API Flask untuk mendapatkan hasil prediksi
-  axios.post('https://growell-flask-api-fkegjceqka-et.a.run.app/api/predict', {
+  axios.post('URL_API_FLASK_CLOUD_RUN/api/predict', {
     name,
     gender,
     age,
@@ -59,4 +60,83 @@ const newStuntingEntry = (req, res) => {
 module.exports = {
   getAllStuntings,
   newStuntingEntry
+};
+*/
+
+const newStuntingEntry = (req, res) => {
+  const { name, gender, age, height, weight } = req.body;
+
+  if (!name || !gender || !age || !height || !weight) {
+    res.status(400).json({ error: true, message: 'Missing required fields in request body' });
+    return;
+  }
+
+  const query = `INSERT INTO stuntings (name, gender, age, height, weight) VALUES ('${name}','${gender}','${age}','${height}','${weight}')`;
+  const values = [name, gender, age, height, weight];
+
+  sql.query(query, values, (err, result) => {
+    if (err) {
+      console.error('Error adding stunting:', err);
+      res.status(500).json({ error: true, message: 'Internal Server Error' });
+    } else {
+      const newStuntingEntry = {
+          name,
+          gender,
+          age,
+          height,
+          weight
+      };
+      res.status(201).json({ error: false, message: 'success', uploadResult: newStuntingEntry, result });
+    }
+  });
+};
+
+const statusStunting = async (req, res) => {
+  const { name, gender, age, height, weight } = req.body;
+
+  // Perform the prediction based on the newStuntingEntry
+  try {
+    const response = await axios.post('https://growell-flask-api-fkegjceqka-et.a.run.app/api/predict', {
+      name,
+      gender,
+      age,
+      height,
+      weight
+    });
+
+  const status = response.data.status;
+
+    const query = `UPDATE stuntings SET status = ${status}`;
+    sql.query(query, (err, result) => {
+      if (err) {
+        console.error('Error updating status:', err);
+        res.status(500).json({ error: true, message: 'Internal Server Error' });
+      } else {
+        const newStatusEntry = {
+          id: result.insertid,
+          name,
+          gender,
+          age,
+          height,
+          weight
+        };
+
+        res.status(200).json({
+          error: false,
+          message: 'Success',
+          uploadResult: newStatusEntry,
+          status: status,
+        });
+      }
+    });
+  } catch (error) {
+    console.error('Error making status request:', error);
+    res.status(500).json({ error: true, message: 'Error Making Status' });
+  }
+};
+
+module.exports = {
+  getAllStuntings,
+  newStuntingEntry,
+  statusStunting
 };
