d58e1e93adf99edbebc44a0e9bb887bb1a85eab1
nnbilarch <125783798+nnbilarch@users.noreply.github.com>
2023-06-15 08:08:56 +0700

    update host database

--- Cloud Computing/stunting-api-express/config/database.js
+++ Cloud Computing/stunting-api-express/config/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 const { promisify } = require("util");
 const connection = mysql.createConnection({
-    host     : '34.101.170.151',
+    host     : '34.101.168.145',
     user     : 'root',
-    password : 'stunting123',
+    password : '***',
     database : 'db-stunting'
 })
 connection.connect((err) => {
