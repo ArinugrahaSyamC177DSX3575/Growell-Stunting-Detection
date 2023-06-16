6a4a136f974b54d10560d66fbefac7cc56b45672
nnbilarch <125783798+nnbilarch@users.noreply.github.com>
2023-06-16 11:56:32 +0700

    Edit code

--- Cloud Computing/stunting-api-flask/app.py
+++ Cloud Computing/stunting-api-flask/app.py
@@ -13,52 +13,44 @@ scaler = StandardScaler()
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
-    data = request.json
+    try:
+        data = request.json
+        
+        gender = data['gender']
+        age = data['age']
+        height = data['height']
+        weight = data['weight']
+
+        if not all([gender, age, height, weight]):
+            return jsonify(error='Missing required fields'), 400
+        
+        age_height_weight = np.array([age, height, weight], dtype=float)
+        scaler.fit(age_height_weight.reshape(-1, 1))
+        
+        # Data Preprocessing
+        gender_encoded = np.where(gender == 'L', [0.0, 1.0], [1.0, 0.0])
+        
+        scaled_age_height_weight = scaler.transform(age_height_weight.reshape(-1, 1)).flatten()
+
+        input_data = np.concatenate([gender_encoded, scaled_age_height_weight])
+        input_data = input_data.reshape(1, -1)
+        input_data = tf.convert_to_tensor(input_data, dtype=tf.float32)
+
+        # Data Prediction 
+        prediction = model.predict(input_data)
+
+        if prediction > 0.5:
+            status = 'tidak stunting'
+        else:
+            status = 'stunting'
+        
+        response = {
+            'status': status
+        }
+
+        return jsonify(response)
     
-    name = data['name']
-    gender = data['gender']
-    age = data['age']
-    height = data['height']
-    weight = data['weight']
-
-    if not all([name, gender, age, height, weight]):
-        return jsonify(error='Missing required fields'), 400
-    
-    gender_encoded = np.where(gender == 'L', [0.0, 1.0], [1.0, 0.0])
-
-    age_height_weight = np.array([age, height, weight], dtype=float)
-
-    scaled_age_height_weight = scaler.transform(age_height_weight.reshape(-1, 1)).flatten()
-
-    input_data = np.concatenate([gender_encoded, scaled_age_height_weight])
-
-    input_data = input_data.reshape(1, -1)
-
-    input_data = tf.convert_to_tensor(input_data, dtype=tf.float32)
-
-    prediction = model.predict(input_data)
-
-    if prediction > 0.5:
-        status = 'tidak stunting'
-    else:
-        status = 'stunting'
-    
-    url = 'https://growell-express-api-fkegjceqka-et.a.run.app/api/statusstunting'
-    payload = {
-        'name': name,
-        'gender': gender,
-        'age': age,
-        'height': height,
-        'weight': weight,
-        'status': status
-    }
-    response = requests.post(url, json=payload)
-    
-    if response.status_code == 201:
-        # Jika permintaan berhasil, mengembalikan respons dari API Express sebagai respons dari API Flask
-        return jsonify(response.json())
-    else:
-        # Jika permintaan gagal, mengembalikan pesan error
+    except Exception:
         return jsonify(error='Failed to save stunting entry'), 500
     
 if __name__ == '__main__':
