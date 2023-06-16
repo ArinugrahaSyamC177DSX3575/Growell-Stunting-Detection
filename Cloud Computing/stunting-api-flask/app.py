e0101d484b73a7f5b6d95a15c8998af7a836c503
AnggaSyam <128708680+ArinugrahaSyamC177DSX3575@users.noreply.github.com>
2023-06-16 11:39:58 +0800

    Update app.py
    add pre-processing and change the endpoint

--- Cloud Computing/stunting-api-flask/app.py
+++ Cloud Computing/stunting-api-flask/app.py
@@ -1,21 +1,20 @@
 from flask import Flask, request, jsonify
-import requests
 import numpy as np
 from tensorflow import keras
 import requests
-
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
-# Load model.h5
-model = keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('model.h5')
 
-# Endpoint untuk menerima permintaan dari aplikasi mobile
+scaler = StandardScaler()
+
 @app.route('/api/predict', methods=['POST'])
 def predict():
     data = request.json
     
-    # Mengambil data dari permintaan
     name = data['name']
     gender = data['gender']
     age = data['age']
@@ -25,17 +24,26 @@ def predict():
     if not all([name, gender, age, height, weight]):
         return jsonify(error='Missing required fields'), 400
     
-    # Melakukan prediksi menggunakan model.h5
-    input_data = np.array([[gender, age, height, weight]])
+    gender_encoded = np.where(gender == 'L', [0.0, 1.0], [1.0, 0.0])
+
+    age_height_weight = np.array([age, height, weight], dtype=float)
+
+    scaled_age_height_weight = scaler.transform(age_height_weight.reshape(-1, 1)).flatten()
+
+    input_data = np.concatenate([gender_encoded, scaled_age_height_weight])
+
+    input_data = input_data.reshape(1, -1)
+
+    input_data = tf.convert_to_tensor(input_data, dtype=tf.float32)
+
     prediction = model.predict(input_data)
-    
+
     if prediction > 0.5:
         status = 'tidak stunting'
     else:
         status = 'stunting'
     
-    # Melakukan permintaan POST ke API Express untuk menyimpan data ke database
-    url = 'https://growell-express-api-fkegjceqka-et.a.run.app/api/newstuntingentry'
+    url = 'https://growell-express-api-fkegjceqka-et.a.run.app/api/statusstunting'
     payload = {
         'name': name,
         'gender': gender,
